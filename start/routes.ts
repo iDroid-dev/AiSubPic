05de570cb9ac182b1034463a5b914309bfc29b1f
Евгений Ахметов <akhmetovyevgeniy@gmail.com>
2026-01-10 20:02:38 +0500

    10.01.2026 Bot Edit4выаввапfgfsdadsвампва

--- start/routes.ts
+++ start/routes.ts
@@ -27,7 +27,7 @@ router.group(() => {
    
 
   // === ПОЛЬЗОВАТЕЛИ ===
-  router.get('/users', [UsersController, 'index']).as('admin.users') // Список
+  router.get('/users', [UsersController, 'index']).as('admin.users')  
   router.get('/users/:id/edit', [UsersController, 'edit']).as('admin.users.edit')
   router.post('/users/:id/credits', [UsersController, 'addCredits']).as('admin.users.credits')
 
